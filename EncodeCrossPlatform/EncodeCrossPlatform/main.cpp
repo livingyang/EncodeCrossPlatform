6cea7137e4a56fd7aadafd27c1e24492eb4d7b44
livingyang <lhm.cold@163.com>
2012-12-07 10:25:57 +0800

    Add: init rope

---
+++ EncodeCrossPlatform/EncodeCrossPlatform/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  EncodeCrossPlatform
+//
+//  Created by 中 青宝 on 12-12-7.
+//  Copyright (c) 2012年 中 青宝. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n" << "中文测试1\n" << "中文测试2" << std::endl;
+    return 0;
+}
+
